388e2cabc9a7dd3f9675c54a76574d6dfb9840e7
Jake Mansfield <jakesamuelmansfield@gmail.com>
2017-02-10 10:58:59 +0000

    functionality complete

--- calculator app/ViewController.swift
+++ calculator app/ViewController.swift
@@ -8,7 +8,17 @@
 
 import UIKit
 
+
+
+
 class ViewController: UIViewController {
+    
+    var Num0 = Int()
+    var Num2 = Int()
+    
+    
+    
+    @IBOutlet weak var Answer: UILabel!
 
     @IBOutlet weak var numberlabel1: UILabel!
     
@@ -16,60 +26,121 @@ class ViewController: UIViewController {
     @IBOutlet weak var numberlabel2: UILabel!
     
     
-    @IBAction func button1(_ sender: UIButton) {
+    @IBAction func btn1(_ sender: Any) {
+        if Int(1) == Num0 + Num2{
+            Answer.text = "Well Done!"
+        }else{
+            Answer.text = "Nah"
+        }
     }
-        
     
     @IBAction func button2(_ sender: UIButton) {
-    }
+        if Int(2) == Num0 + Num2{
+            Answer.text = "Well Done!"
+        }else{
+            Answer.text = "Nah"
+        }    }
     
     
     @IBAction func button3(_ sender: UIButton) {
+        if Int(3) == Num0 + Num2{
+            Answer.text = "Well Done!"
+        }else{
+            Answer.text = "Nah"
+        }
+        
     }
     
     
     @IBAction func button4(_ sender: UIButton) {
+        if Int(4) == Num0 + Num2{
+            Answer.text = "Well Done!"
+        }else{
+            Answer.text = "Nah"
+        }
+        
     }
     
     
     @IBAction func button5(_ sender: UIButton) {
+        if Int(5) == Num0 + Num2{
+            Answer.text = "Well Done!"
+        }else{
+            Answer.text = "Nah"
+        }
+        
     }
     
     
     @IBAction func button6(_ sender: UIButton) {
+        if Int(6) == Num0 + Num2{
+            Answer.text = "Well Done!"
+        }else{
+            Answer.text = "Nah"
+        }
+        
     }
     
     
     @IBAction func button7(_ sender: UIButton) {
+        if Int(7) == Num0 + Num2{
+            Answer.text = "Well Done!"
+        }else{
+            Answer.text = "Nah"
+        }
+        
     }
     
     
     @IBAction func button8(_ sender: UIButton) {
+        if Int(8) == Num0 + Num2{
+            Answer.text = "Well Done!"
+        }else{
+            Answer.text = "Nah"
+        }
+        
     }
    
     
     @IBAction func button9(_ sender: UIButton) {
+        if Int(9) == Num0 + Num2{
+            Answer.text = "Well Done!"
+        }else{
+            Answer.text = "Nah"
+        }
+        
     }
     
     
     @IBAction func button10(_ sender: UIButton) {
+        if Int(10) == Num0 + Num2{
+            Answer.text = "Well Done!"
+        }else{
+            Answer.text = "Nah"
+        }
+        
     }
     
     
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let Num = Int(arc4random_uniform(UInt32(6) - UInt32(1)) + UInt32(1))
+        self.view.backgroundColor = UIColor.lightGray
+
         
-        let Num2 = Int(arc4random_uniform(UInt32(6) - UInt32(1)) + UInt32(1))
+        Num0 = Int(arc4random_uniform(UInt32(6) - UInt32(1)) + UInt32(1))
         
-        numberlabel1.text = String(Num)
+        Num2 = Int(arc4random_uniform(UInt32(6) - UInt32(1)) + UInt32(1))
+        
+        numberlabel1.text = String(Num0)
         numberlabel2.text = String(Num2)
         
-        let answer = Int(Num + Num2)
         
         
+        
+       
         
         
     }
